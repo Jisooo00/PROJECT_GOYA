31372b54c55ed26e69f0cb29408ba9e004d1a86b
Jisooo00 <jisooopark34@gmail.com>
2024-07-08 14:30:52 +0900

    서버주소 변경

--- Project/Client/projectGOYA/Assets/Scripts/Network/WebReq.cs
+++ Project/Client/projectGOYA/Assets/Scripts/Network/WebReq.cs
@@ -22,7 +22,7 @@ public class WebReq : MonoBehaviour
 		}
 	}
 
-	string url = "http://43.201.26.129:8888/";
+	string url = "http://13.125.205.86:8888/";
 
 	long user_uid = 0;
 	bool sending = false;
